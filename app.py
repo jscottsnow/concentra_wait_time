01828acdc2cd3b2feff58a0ecf7a90381d3be4ba
jscottsnow <jscottsnow84@gmail.com>
2025-03-02 20:46:57 -0500

    switched to eventlet for production stability

--- app.py
+++ app.py
@@ -65,4 +65,4 @@ import eventlet
 import eventlet.wsgi
 
 if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=5000)
+    eventlet.wsgi.server(eventlet.listen(('0.0.0.0', 5000)), app)
